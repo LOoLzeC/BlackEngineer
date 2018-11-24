9103e76ddb457751b94efd7046a8e7d94bf4f064
Deray <33969267+LOoLzeC@users.noreply.github.com>
2018-11-24 22:07:26 +0700

    Add files via upload

---
+++ raw/banner.py
@@ -0,0 +1,39 @@
+#-*-coding:utf-8-*-
+# Module Banner
+# BlackEngineer By Deray
+# Report bug on my other sosmed
+# https://facebook.com/achmadluthfi.hadi.3
+# https://instagram.com/reyy05_
+
+W = '\033[1;37m' 
+N  = '\033[0m' 
+R = '\033[31m'
+G  = '\033[1;32m' 
+O  = '\033[1;37m\033[33m'
+B  = '\033[34m' 
+P  = '\033[35m' 
+C  = '\033[36m' 
+import cacheclean
+cacheclean.cache()
+blackEngineer_Banner = """ \033[1;36m
+┌═══════════════════════════════════════════════════┐
+█                                                   █
+█             Happy Hunting ¯\_(ツ)_/¯              █ \033[1;91m
+  ___   _        _     ___                            
+ | _ ) | |  __  | |__ | __|  _ _    __ _   _ _    _ _ 
+ | _ \ | | / _| | / / | _|  | ' \  / _` | | ' \  | '_|
+ |___/ |_| \__| |_\_\ |___| |_||_| \__, | |_||_| |_|  
+                                   |___/\033[1;36m              
+█                                                   █
+█              BlackEngineer By Deray               █
+└═══════════════════════════════════════════════════┘     \n \033[1;91m
+ {}1). {}Auto Like Facebook (need lot of access token)
+ {}2). {}Mass Generate Access Token Facebook
+ {}3). {}Mass Spammer Comment Facebook
+ {}4). {}Delete A Friends From Other City
+ {}5). {}Search Friends
+ {}6). {}Get Friends Post ID
+ {}7). {}Update Resources
+ {}8). {}Info.
+ {}9). {}Byee.{}
+ """.format(R,O,R,O,R,O,R,O,R,O,R,O,R,O,R,O,R,O,W)
